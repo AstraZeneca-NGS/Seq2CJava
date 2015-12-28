6641419fde1bbe57101d094abca60ce2cce45ccf
Victor Kirst <viktor_kirst@epam.com>
2015-12-28 18:45:06 +0300

    Refactoring

--- src/main/java/com/astrazeneca/seq2c/Seq2cov.java
+++ src/main/java/com/astrazeneca/seq2c/Seq2cov.java
@@ -112,6 +112,24 @@ public class Seq2cov {
         return false; // "grch";
     }
 
+
+    private static class Seq2covGene {
+
+        public final List<int[]> CDS = new ArrayList<>();
+        public final String chr;
+        public final String gene;
+
+        public Seq2covGene(String chr, String gene, int[] CDS) {
+            this.chr = chr;
+            this.gene = gene;
+            this.CDS.add(CDS);
+        }
+
+        public void addCDS(int[] CDS) {
+            this.CDS.add(CDS);
+        }
+    }
+
     private static class GeneCtx {
 
         final ArrayList<GeneX> gns;
